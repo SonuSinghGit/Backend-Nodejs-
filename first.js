6e0a7c19d833c147074e1de6117c8ed46477c07f
Sonu Kumar <sonucse103@gmail.com>
2025-12-07 23:58:11 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,7 @@
+function greet(name){
+    return name;
+}
+const res= greet("sonu")
+console.log('====================================');
+console.log(res);
+console.log('====================================');
