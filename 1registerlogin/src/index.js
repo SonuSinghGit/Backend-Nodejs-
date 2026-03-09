aea455ad1c0e51bec34368ac73ded60d7c7bbb14
Sonu Kumar <sonucse103@gmail.com>
2026-03-09 22:30:38 +0530

    removed node_modules

---
+++ 1registerlogin/src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/config.js";
+
+dotenv.config(); 
+
+const PORT = process.env.PORT || 9000;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on: ${PORT}`);
+});
+
+app.get("/",(res,req)=>{
+    res.send("welcome to node js backend")
+})
+
+connectDB();
