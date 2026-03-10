fc23105b7d13c1248a715eaf6fc76748d0f118dc
Sonu Kumar <sonucse103@gmail.com>
2026-03-10 23:01:51 +0530

    create a login form

--- 1registerandlogin/client/src/App.jsx
+++ 1registerandlogin/client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Register from "./components/Register.jsx";
+import Login from "./components/Login.jsx";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <h1 class="text-3xl font-bold text-center underline">welcome to our plateform</h1>
       <div className="mt-4 text-center">
          <Register/>
+         <Login/>
       </div>
      
     </div>
