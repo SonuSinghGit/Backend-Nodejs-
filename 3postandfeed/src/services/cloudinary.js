ec3ffda449b113efbc554af452c8c96c1f520200
Sonu Kumar <sonucse103@gmail.com>
2026-03-21 23:48:34 +0530

    create a function for upload file

---
+++ 3postandfeed/src/services/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from 'cloudinary'
+import {dotenv} from "dotenv"
+import {fs} from "fs";
+dotenv.config();
+
+cloudinary.config({ 
+  cloud_name:process.env.CLOUDE_NAME, 
+  api_key:process.env.CLOUDE_API_KEY,
+  api_secret:process.env.CLOUDE_API_SECRET
+});
+
+const uploadOnCloudinary = async(localFilePath)=>{
+    try {
+        if(!localFilePath) return null
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        //file has been upload sucessfully
+        console.log("Fil is uploaded on clodinary",response.url);
+        return response;
+    } catch (error) {
+        
+    }
+
+}
